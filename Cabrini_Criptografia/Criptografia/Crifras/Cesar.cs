214f2b3233e63573eae22c2b08f97d955d22f861
Rafael Lillo <andrade_177@hotmail.com>
2015-02-05 21:05:49 -0200

    Add cifra de cesar

---
+++ Cabrini_Criptografia/Criptografia/Crifras/Cesar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criptografia.Crifras
+{
+    public class Cesar
+    {
+        public string Criptografa(string texto, int chave = 3)
+        {
+            return DeOuCriptogrfa(texto, chave );
+        }
+
+        public string Decriptografa(string texto, int chave = 3)
+        {
+            return DeOuCriptogrfa(texto, (chave * -1));
+        }
+
+        private string DeOuCriptogrfa(string texto,int chave)
+        {
+            var retorno = new StringBuilder();
+
+            for (int i = 0; i < texto.Length; i++)
+            {
+                int valorConversao = (int)texto[i] + chave;
+                valorConversao = valorConversao % 256;
+                retorno.Append(valorConversao);
+            }
+
+            return retorno.ToString();
+        }
+    }
+}
